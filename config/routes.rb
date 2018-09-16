7e6d6d18072c90a4eae0e72782f1d9989fa3afeb
전준호 <>
2018-09-16 14:24:26 +0900

    tt

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'kakao/callback'
 
   get 'kakao/index'
+  get 'kakao/t'
   root 'kakao#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
