cd8174718d5d097a35816a6b5f70c45aefc31ece
jeonjoonho-nbt <jeon.joonho@nbt.com>
2018-09-19 15:34:06 +0900

    a

--- lib/kakao_share_logger.rb
+++ lib/kakao_share_logger.rb
@@ -7,20 +7,17 @@ class KakaoShareLogger
   DEFAULT_BQ_FOLDER_PATH = 'log/es_log/'
 
   def initialize(folder_path= DEFAULT_BQ_FOLDER_PATH, log_name=DEFAULT_BQ_LOG_FILE_NAME)
-    ActiveSupport::JSON.initialize
     @log_path = folder_path + log_name
     unless File.exist? @log_path
       FileUtils::mkdir_p folder_path
       File.open(@log_path, 'w').close
     end
-    @logger = ActiveSupport::BufferedLogger.new(@log_path)
+    @logger = ActiveSupport::TaggedLogging.new(Logger.new(@log_path))
   end
 
-  private
   def log(json_log)
-    log_data = ActiveSupport::JSON.stringify(json_log)
-    @logger.info log_data
+    @logger.info json_log
   rescue => e
-    puts('error')
+    puts("error => #{e}")
   end
 end
