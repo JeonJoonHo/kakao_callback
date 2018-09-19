8ef61da3e67d5debe7f8b5c503e9f1ce16fab418
jeonjoonho-nbt <jeon.joonho@nbt.com>
2018-09-19 15:49:05 +0900

    mm

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module KakaoCallback
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.paths.add Rails.root.join('lib').to_s, eager_load: true
+
     config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
